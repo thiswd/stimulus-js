bfe41ec304877cd626188975e42ad11fe54fe2b1
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-26 00:04:58 -0300

    feat: register disable button controller

--- javascript/application.js
+++ javascript/application.js
@@ -1,2 +1,5 @@
 import { Application, Controller } from 'stimulus';
 window.Stimulus = Application.start()
+
+import DisableButtonController from "./controllers/disable_button_controller.js"
+Stimulus.register("disable-button", DisableButtonController)
