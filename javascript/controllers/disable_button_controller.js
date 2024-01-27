6615e07cf352d99676866e66b94f67ed62820207
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-26 21:19:17 -0300

    feat: reset button

--- javascript/controllers/disable_button_controller.js
+++ javascript/controllers/disable_button_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["button", "link"]
 
   // Método padrão chamado sempre que o controller se conecta com o DOM
   connect() {
@@ -10,5 +10,11 @@ export default class extends Controller {
 
   disable() {
     this.buttonTarget.setAttribute("disabled", "");
+    this.linkTarget.classList.remove("d-none");
+  }
+
+  reset() {
+    this.buttonTarget.removeAttribute("disabled");
+    this.linkTarget.classList.add("d-none");
   }
 }
