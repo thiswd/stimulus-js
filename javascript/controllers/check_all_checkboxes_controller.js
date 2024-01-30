254e84f4e57268fd17dd81259c003667eb55d410
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-29 22:03:55 -0300

    feat: check all checkboxes

--- javascript/controllers/check_all_checkboxes_controller.js
+++ javascript/controllers/check_all_checkboxes_controller.js
@@ -1,8 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = ["checkAll", "checkbox"]
 
   connect() {
     console.log("Hello from check all checkboxes controller");
+  }
+
+  checkAll() {
+    this.checkboxTargets.forEach(checkbox => {
+      checkbox.checked = this.checkAllTarget.checked
+    })
   }
 }
