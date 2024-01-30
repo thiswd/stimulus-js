1e9af6f2b8f396080849f160873d4d597b02a497
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-29 21:45:45 -0300

    feat: setup check all checkboxes controller

--- javascript/application.js
+++ javascript/application.js
@@ -2,7 +2,9 @@ import { Application, Controller } from 'stimulus';
 window.Stimulus = Application.start()
 
 import DisableButtonController from "./controllers/disable_button_controller.js"
-Stimulus.register("disable-button", DisableButtonController)
-
 import MoviesController from "./controllers/movies_controller.js"
+import CheckAllCheckboxesController from "./controllers/check_all_checkboxes_controller.js"
+
+Stimulus.register("disable-button", DisableButtonController)
 Stimulus.register("movies", MoviesController)
+Stimulus.register("check-all", CheckAllCheckboxesController)
