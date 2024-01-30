5a3cb9121f62285865a0e71a576398103fde3046
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-29 22:08:48 -0300

    refactor: clean imports

--- javascript/application.js
+++ javascript/application.js
@@ -1,4 +1,4 @@
-import { Application, Controller } from 'stimulus';
+import { Application } from 'stimulus';
 window.Stimulus = Application.start()
 
 import DisableButtonController from "./controllers/disable_button_controller.js"
