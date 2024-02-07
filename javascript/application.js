9d8e0868d148a78f3d95db0861b035bcfcc74d3b
Thiago Sawada <tms1_tms2@hotmail.com>
2024-02-06 22:17:55 -0300

    feat: register copy to clipboard controller

--- javascript/application.js
+++ javascript/application.js
@@ -4,7 +4,9 @@ window.Stimulus = Application.start()
 import DisableButtonController from "./controllers/disable_button_controller.js"
 import MoviesController from "./controllers/movies_controller.js"
 import CheckAllCheckboxesController from "./controllers/check_all_checkboxes_controller.js"
+import CopyToClipboardController from "./controllers/copy_to_clipboard_controller.js"
 
 Stimulus.register("disable-button", DisableButtonController)
 Stimulus.register("movies", MoviesController)
 Stimulus.register("check-all", CheckAllCheckboxesController)
+Stimulus.register("copy-to-clipboard", CopyToClipboardController)
